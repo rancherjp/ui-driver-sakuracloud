2e531a9fdf25c8c560f591b6cc49eec42dd37c8e
Go Chiba <go.chiba@gmail.com>
2016-10-25 12:17:52 +0900

    Add support account information for Sakura Cloud

--- component/component.js
+++ component/component.js
@@ -1,51 +1,63 @@
+let REGIONS = [
+  "is1a",
+  "is1b",
+  "tk1a",
+];
+
 /* v----- Do not change anything between here
  *       (the DRIVERNAME placeholder will be automatically replaced during build) */
 define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'ember', 'ui/mixins/driver'], function (exports, _ember, _uiMixinsDriver) {
 
   exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
     driverName: '%%DRIVERNAME%%',
+    //sakuracloudConfig: Ember.computed.alias('model.%%DRIVERNAME%%Config'),
+    regionChoices: REGIONS,
+
 /* ^--- And here */
 
     // Write your component here, starting with setting 'model' to a machine with your config populated
     bootstrap: function() {
       let config = this.get('store').createRecord({
         type        : '%%DRIVERNAME%%Config',
-        size        : 512,
+        region      : 'is1a',
+        accessToken : '',
+        secretToken : '',
+        connectedSwitch: '',
       });
 
       this.set('model', this.get('store').createRecord({
         type: 'machine',
         '%%DRIVERNAME%%Config': config,
       }));
-    },
+    }
 
     // Add custom validation beyond what can be done from the config API schema
-    validate() {
+    //validate() {
       // Get generic API validation errors
-      this._super();
-      var errors = this.get('errors')||[];
+      //this._super();
+      //var errors = this.get('errors')||[];
 
       // Add more specific errors
 
       // Check something and add an error entry if it fails:
-      if ( parseInt(this.get('model.%%DRIVERNAME%%Config.size'),10) < 1024 )
-      {
-        errors.push('Size must be at least 1024 MB');
-      }
+      //if ( parseInt(this.get('model.%%DRIVERNAME%%Config.size'),10) < 1024 )
+      //{
+      //  errors.push('Size must be at least 1024 MB');
+      //}
 
       // Set the array of errors for display,
       // and return true if saving should continue.
-      if ( errors.get('length') )
-      {
-        this.set('errors', errors);
-        return false;
-      }
-      else
-      {
-        this.set('errors', null);
-        return true;
-      }
-    },
+      //if ( errors.get('length') )
+      //{
+      //  this.set('errors', errors);
+      //  return false;
+      //}
+      //else
+      //{
+      //  this.set('errors', null);
+      //  return true;
+      //}
+    //},
 
     // Any computed properties or custom logic can go here
   });
