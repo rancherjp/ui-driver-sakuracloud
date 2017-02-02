792e3a2b653d9e2f80c0552b89ece99d84974986
Go Chiba <go.chiba@gmail.com>
2017-02-02 13:13:25 +0900

    Fixed hostname naming rule issue.

--- component/component.js
+++ component/component.js
@@ -20,7 +20,7 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
 
   exports['default'] = _ember['default'].Component.extend(_uiMixinsDriver['default'], {
     driverName: '%%DRIVERNAME%%',
-    //sakuracloudConfig: Ember.computed.alias('model.%%DRIVERNAME%%Config'),
+    %%DRIVERNAME%%Config: Ember.computed.alias('model.%%DRIVERNAME%%Config'),
     regionChoices        : REGIONS,
     diskPlanChoices      : DISKPLAN,
     diskConnectionChoices: DISKCONNECTION,
@@ -55,18 +55,42 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
       });
 
       this.set('model', this.get('store').createRecord({
-        type: 'machine',
+        type: 'host',
         '%%DRIVERNAME%%Config': config,
       }));
-    }
+    },
 
     // Add custom validation beyond what can be done from the config API schema
-    //validate() {
+    validate() {
       // Get generic API validation errors
-      //this._super();
-      //var errors = this.get('errors')||[];
+      this._super();
+      var errors = this.get('errors')||[];
 
       // Add more specific errors
+
+      // Check empty value for access/secret token
+      let accessToken = this.get('%%DRIVERNAME%%Config.accessToken');
+      if ( accessToken == null ) {
+        errors.push('アクセストークンは必須です');
+      }
+
+      let accessTokenSecret = this.get('%%DRIVERNAME%%Config.accessTokenSecret');
+      if ( accessTokenSecret == null ) {
+        errors.push('シークレットトークンは必須です');
+      }
+
+      // Check IP address
+      let privateIp = this.get('%%DRIVERNAME%%Config.privateIp');
+      let gateway   = this.get('%%DRIVERNAME%%Config.gateway');
+      //if ( privateIp.match(/[^0-9].[0-9] ) {
+      //  errors.push('IP アドレスが間違っています');
+      //}
+
+      // Check netmask
+      let netmask = this.get('%%DRIVERNAME%%Config.netmask');
+      //if ( privateIp.match(/[^0-9].[0-9] ) {
+      //  errors.push('ネットマスクが間違っています');
+      //}
 
       // Check something and add an error entry if it fails:
       //if ( parseInt(this.get('model.%%DRIVERNAME%%Config.size'),10) < 1024 )
@@ -76,17 +100,17 @@ define('ui/components/machine/driver-%%DRIVERNAME%%/component', ['exports', 'emb
 
       // Set the array of errors for display,
       // and return true if saving should continue.
-      //if ( errors.get('length') )
-      //{
-      //  this.set('errors', errors);
-      //  return false;
-      //}
-      //else
-      //{
-      //  this.set('errors', null);
-      //  return true;
-      //}
-    //},
+      if ( errors.get('length') )
+      {
+        this.set('errors', errors);
+        return false;
+      }
+      else
+      {
+        this.set('errors', null);
+        return true;
+      }
+    },
 
     // Any computed properties or custom logic can go here
   });
